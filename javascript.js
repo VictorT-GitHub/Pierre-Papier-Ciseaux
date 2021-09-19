70b32cd31321e5a58962eb5f35cd495a32bc8987
Victor Tardif <victor.tardif1@gmail.com>
2021-09-19 13:58:11 +0200

    commit 2: Add style (light mode responsive)

--- javascript.js
+++ javascript.js
@@ -20,22 +20,27 @@ else {
     scoreOrdinateur++;
     resultat = "Vous avez perdu !";
 }
-score.innerHTML = `Scores : joueur[${scoreJoueur1}] - ordinateur[${scoreOrdinateur}]`;
-infos.innerHTML = `Vous avez joué : ${e.target.innerHTML}<br>L'ordinateur a joué : ${choixPossible[choixOrdinateur]}<br>${resultat}`;
-}
-
+score.innerHTML = `Scores :<br>joueur[${scoreJoueur1}] - ordinateur[${scoreOrdinateur}]`;
+infos.innerHTML = `Vous avez joué : <strong>${e.target.innerHTML}</strong><br>L'ordinateur a joué : <strong>${choixPossible[choixOrdinateur]}</strong><br><strong>${resultat}</strong>`;
+};
 
 // Creation du <main> 
 const newMain = document.createElement("main");
 
+// Creation <h1>
+const newh1 = document.createElement('h1');
+newh1.innerHTML = "Pierre Papier Ciseaux"
+
 // Creation des 3 <div>
 const newDivScore = document.createElement("div");
 const newDivBtn = document.createElement("div");
+newDivBtn.classList.add("div-btn")
 const newDivInfos = document.createElement("div");
 
 // Creation <p> pour les scores + Deplacement dans newDivScores
 let score = document.createElement("p");
-score.innerHTML = "Scores : joueur[0] - ordinateur[0]";
+score.classList.add("scores");
+score.innerHTML = "Scores :<br>joueur[0] - ordinateur[0]";
 newDivScore.appendChild(score);
 
 // Creation des <button> + Creation [addEventListener] + Deplacement dans newDivBtn
@@ -50,8 +55,9 @@ for (let i = 0; i < choixPossible.length; i++) {
 
 // Creation <p> pour infos destinées a l'utilisateur
 let infos = document.createElement("p");
-infos.innerHTML = `Vous avez joué :<br>L'ordinateur a joué :` 
-newDivInfos.appendChild(infos)
+infos.classList.add("infos");
+infos.innerHTML = `Vous avez joué :<br>L'ordinateur a joué :`;
+newDivInfos.appendChild(infos);
 
 // Deplacement des 3 newDiv dans newMain
 newMain.appendChild(newDivScore);
@@ -59,4 +65,7 @@ newMain.appendChild(newDivBtn);
 newMain.appendChild(newDivInfos);
 
 // Deplacement de newMain dans <body>
-document.body.appendChild(newMain);
+document.body.insertBefore(newMain, document.querySelector("footer"));
+
+// Deplacement de newH1 dans <header>
+document.querySelector("header").appendChild(newh1);
