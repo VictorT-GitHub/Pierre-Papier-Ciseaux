5b2dcde95e1df68fe4b0b954ad4c0e77ad76fcba
Victor Tardif <victor.tardif1@gmail.com>
2022-02-10 00:38:03 +0100

    MAJ javascript: try to fix all this shit code

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 Simple Chifoumi game with scores and dark/light mode switch system.
 
+You can play with the buttons or with keyboard keys.
+
 **JavaScript** - **CSS3** - **HTML5**
 
 [Try it on the GitHub page!](https://victort-github.github.io/Pierre-Papier-Ciseaux/)
+
+| Key | Control                      |
+| --- | ---------------------------- |
+| & 1 | Press `pierre` button        |
+| é 2 | Press `papier` button        |
+| " 3 | Press `ciseaux` button       |
+| r   | Reset scores and clean board |
--- javascript.js
+++ javascript.js
@@ -6,76 +6,67 @@ let resultat = "";
 
 // Function shifumi BUTTONS
 function shifumi(e) {
-    const choixJoueur = choixPossible.indexOf(e.target.innerHTML)
-    const choixOrdinateur = Math.floor(Math.random() * choixPossible.length);
+  const choixJoueur = choixPossible.indexOf(e.target.innerHTML);
+  const choixOrdinateur = Math.floor(Math.random() * choixPossible.length);
 
-if (choixJoueur == choixOrdinateur) {
+  if (choixJoueur == choixOrdinateur) {
     resultat = "Egalité !";
-}
-else if ((choixJoueur == 1 && choixOrdinateur == 0 ) || (choixJoueur == 2 && choixOrdinateur == 1 ) || (choixJoueur == 0 && choixOrdinateur == 2 )) {
+  } else if (
+    (choixJoueur == 1 && choixOrdinateur == 0) ||
+    (choixJoueur == 2 && choixOrdinateur == 1) ||
+    (choixJoueur == 0 && choixOrdinateur == 2)
+  ) {
     scoreJoueur1++;
-    resultat = 'Vous avez gagné !';
-}
-else {
+    resultat = "Vous avez gagné !";
+  } else {
     scoreOrdinateur++;
-    resultat = 'Vous avez perdu !';
+    resultat = "Vous avez perdu !";
+  }
+  score.innerHTML = `Scores :<br>joueur[${scoreJoueur1}] - ordinateur[${scoreOrdinateur}]`;
+  infos.innerHTML = `Vous avez joué : <strong>${e.target.innerHTML}</strong><br>L'ordinateur a joué : <strong>${choixPossible[choixOrdinateur]}</strong><br><strong>${resultat}</strong>`;
 }
-score.innerHTML = `Scores :<br>joueur[${scoreJoueur1}] - ordinateur[${scoreOrdinateur}]`;
-infos.innerHTML = `Vous avez joué : <strong>${e.target.innerHTML}</strong><br>L'ordinateur a joué : <strong>${choixPossible[choixOrdinateur]}</strong><br><strong>${resultat}</strong>`;
-};
 
 // Function shifumi KEYPRESS
 function shifumiKey(e) {
-if (e.key === "&" || e.key === "é" || e.key === '"') {
+  if (e.key === "&" || e.key === "é" || e.key === '"') {
     const choixOrdinateur = Math.floor(Math.random() * choixPossible.length);
     let choixJoueur = "";
-    
+
     if (e.key === "&") {
-        choixJoueur =  0;
+      choixJoueur = 0;
+    } else if (e.key === "é") {
+      choixJoueur = 1;
+    } else if (e.key === '"') {
+      choixJoueur = 2;
     }
-    else if (e.key === "é") {
-        choixJoueur =  1;
+
+    if (choixJoueur == choixOrdinateur) {
+      resultat = "Egalité !";
+    } else if (
+      (choixJoueur == 1 && choixOrdinateur == 0) ||
+      (choixJoueur == 2 && choixOrdinateur == 1) ||
+      (choixJoueur == 0 && choixOrdinateur == 2)
+    ) {
+      scoreJoueur1++;
+      resultat = "Vous avez gagné !";
+    } else {
+      scoreOrdinateur++;
+      resultat = "Vous avez perdu !";
     }
-    else if (e.key === '"') {
-        choixJoueur =  2;
-    }
-    else if (e.key === "r") {
-        infos.innerHTML = "bite";
-    }
-if (choixJoueur == choixOrdinateur) {
-    resultat = "Egalité !";
-}
-else if ((choixJoueur == 1 && choixOrdinateur == 0 ) || (choixJoueur == 2 && choixOrdinateur == 1 ) || (choixJoueur == 0 && choixOrdinateur == 2 )) {
-    scoreJoueur1++;
-    resultat = "Vous avez gagné !";
-}
-else {
-    scoreOrdinateur++;
-    resultat = "Vous avez perdu !";
-}
-score.innerHTML = `Scores :<br>joueur[${scoreJoueur1}] - ordinateur[${scoreOrdinateur}]`;
-infos.innerHTML = `Vous avez joué : <strong>${choixPossible[choixJoueur]}</strong><br>L'ordinateur a joué : <strong>${choixPossible[choixOrdinateur]}</strong><br><strong>${resultat}</strong>`;
-    }
-else if (e.key === "r") { 
+    score.innerHTML = `Scores :<br>joueur[${scoreJoueur1}] - ordinateur[${scoreOrdinateur}]`;
+    infos.innerHTML = `Vous avez joué : <strong>${choixPossible[choixJoueur]}</strong><br>L'ordinateur a joué : <strong>${choixPossible[choixOrdinateur]}</strong><br><strong>${resultat}</strong>`;
+  } else if (e.key === "r") {
     scoreJoueur1 = 0;
     scoreOrdinateur = 0;
     score.innerHTML = `Scores :<br>joueur[${scoreJoueur1}] - ordinateur[${scoreOrdinateur}]`;
-    infos.innerHTML = ""
+    infos.innerHTML = "";
+  }
 }
-};
-
-// Creation <h1> + Deplacement dans <header>
-const newh1 = document.createElement('h1');
-newh1.innerHTML = "Pierre Papier Ciseaux"
-document.querySelector("header").appendChild(newh1);
-
-// Creation du <main> 
-const newMain = document.createElement("main");
 
 // Creation des 3 <div>
 const newDivScore = document.createElement("div");
 const newDivBtn = document.createElement("div");
-newDivBtn.classList.add("div-btn")
+newDivBtn.classList.add("div-btn");
 const newDivInfos = document.createElement("div");
 
 // Creation <p> pour les scores + Deplacement dans newDivScores
@@ -86,13 +77,13 @@ newDivScore.appendChild(score);
 
 // Creation des <button> + Creation [addEventListener] + Deplacement dans newDivBtn
 for (let i = 0; i < choixPossible.length; i++) {
-    const newBtn = document.createElement("button");
-    newBtn.innerHTML = `${choixPossible[i]}`;
-    newBtn.classList.add("btn");
-    newBtn.addEventListener('click', shifumi);
-    
-    newDivBtn.appendChild(newBtn);
-};
+  const newBtn = document.createElement("button");
+  newBtn.innerHTML = `${choixPossible[i]}`;
+  newBtn.classList.add("btn");
+  newBtn.addEventListener("click", shifumi);
+
+  newDivBtn.appendChild(newBtn);
+}
 
 // Creation <p> pour infos destinées a l'utilisateur
 let infos = document.createElement("p");
@@ -100,39 +91,36 @@ infos.classList.add("infos");
 infos.innerHTML = `Vous avez joué :<br>L'ordinateur a joué :`;
 newDivInfos.appendChild(infos);
 
-// Deplacement des 3 newDiv dans newMain
-newMain.appendChild(newDivScore);
-newMain.appendChild(newDivBtn);
-newMain.appendChild(newDivInfos);
-
-// Deplacement de newMain dans <body>
-document.body.insertBefore(newMain, document.querySelector("footer"));
+// Deplacement des 3 newDiv dans le main
+const theMain = document.querySelector("main");
+theMain.appendChild(newDivScore);
+theMain.appendChild(newDivBtn);
+theMain.appendChild(newDivInfos);
 
 // Creation BUTTON Light/Dark Theme
 const newThemeBtn = document.createElement("button");
 newThemeBtn.classList.add("themeBtn");
 newThemeBtn.classList.add("darkTheme");
 newThemeBtn.innerHTML = "Go Dark";
-    // Ajout [addEventListener] + [function] flechee qui switch theme dark/light
-    const lesBtn = document.querySelectorAll(".btn");
-    newThemeBtn.addEventListener('click', () => {
-        newThemeBtn.classList.toggle("darkTheme");
-        // condition pour changer text "go dark" -> "go light"
-        if (!newThemeBtn.classList.contains("darkTheme")){
-            newThemeBtn.innerHTML = "Go Light";
-        } else if (newThemeBtn.classList.contains("darkTheme")){
-            newThemeBtn.innerHTML = "Go Dark";
-            }
-        
-        document.body.classList.toggle("darkTheme");
-        
-        for (let fdp of lesBtn){
-            fdp.classList.toggle("darkTheme");
-        }
-}
-)
+// Ajout [addEventListener] + [function] flechee qui switch theme dark/light
+const lesBtn = document.querySelectorAll(".btn");
+newThemeBtn.addEventListener("click", () => {
+  newThemeBtn.classList.toggle("darkTheme");
+  // condition pour changer text "go dark" -> "go light"
+  if (!newThemeBtn.classList.contains("darkTheme")) {
+    newThemeBtn.innerHTML = "Go Light";
+  } else if (newThemeBtn.classList.contains("darkTheme")) {
+    newThemeBtn.innerHTML = "Go Dark";
+  }
+
+  document.body.classList.toggle("darkTheme");
+
+  for (let fdp of lesBtn) {
+    fdp.classList.toggle("darkTheme");
+  }
+});
 document.querySelector("aside").appendChild(newThemeBtn);
 
 // KeyPress EVENT
-// Creation [addEventListener] sur <body> 
+// Creation [addEventListener] sur <body>
 document.body.addEventListener("keypress", shifumiKey);
